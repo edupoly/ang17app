c807a06bb86ca8b54c01e81f407881266c6b9343
edupoly <info.edupoly@gmail.com>
2022-10-17 10:03:35 +0530

    databinding event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang17app';
+  firstname='praveen';
+  countries = ['india','nepal','namibia','hongkong','malasia','combodia','bankok']
+  addCountry(){
+    alert("HI")
+  }
 }
+
+// databinding
+// webpage
+// // structure //html
+// // presentation//css
+// // logic // js
+// // data  // fetched from server// js
