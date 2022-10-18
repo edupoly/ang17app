190592c3ae3cba686047bacd6fa712d99594898f
edupoly <info.edupoly@gmail.com>
2022-10-18 10:25:41 +0530

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tasks=['clean washing machine','repair mobile','hire hr','search good place'];
-  technologies=['html','css','js','nodejs','reactjs','angular','express','bootstrap']
-  selectedTechs:any=[];
-  gender=null;
-  newtask='';
-  addTask(){
-    this.tasks.push(this.newtask);
-  }
-  xyz(e:any){
-    if(e.target.checked){
-      this.selectedTechs.push(e.target.value)
-    }
-    else{
-      var i = this.selectedTechs.indexOf(e.target.value);
-      this.selectedTechs.splice(i,1)
-    }
-
-  }
+  
 }
