ea577bc14336cfcfc08575b6522ae2ed720eecd9
edupoly <info.edupoly@gmail.com>
2022-10-19 09:30:22 +0530

    propbind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  selectedItem='';
+  sitem(item:any){
+    this.selectedItem=item;
+  }
 }
