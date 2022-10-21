826091b596d468ce9f3ca5d43af3ade84199ee87
edupoly <info.edupoly@gmail.com>
2022-10-21 10:46:39 +0530

    parent child input property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang17app';
+  firstname = 'praveen';
+  myimage="https://s3-ap-southeast-1.amazonaws.com/tv-prod/member/photo/1787281-large.jpg";
+  imageWidth=200
 }
