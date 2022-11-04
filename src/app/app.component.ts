83055f540581bbc054c0b02b7cf371ec76c69ca9
edupoly <info.edupoly@gmail.com>
2022-11-04 10:29:26 +0530

    crud books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang17app';
-  countries:any=[]
-  constructor(public http:HttpClient){
-    this.http.get("https://restcountries.com/v2/all")
+  books:any=[]
+  newbook={
+    title:'',
+    author:'',
+    id:''
+  }
+  editflag=false;
+  getBooks(){
+    this.http.get("http://localhost:3000/books")
     .subscribe((res)=>{
-      this.countries=res;
+      this.books=res;
+      console.log(this.books)
     })
+  }
+  addBook(){
+    this.http.post("http://localhost:3000/books",this.newbook)
+    .subscribe((data)=>{
+      this.getBooks()
+    })
+  }
+  editBook(book:any){
+    this.editflag=true;
+    this.newbook=book;
+  }
+  updateBook(){
+    this.http.put(`http://localhost:3000/books/${this.newbook.id}`,this.newbook)
+    .subscribe((data)=>{
+      this.editflag=false;
+      this.newbook={
+        title:'',
+        author:'',
+        id:''
+      }
+      this.getBooks()
+    })
+  }
+  deleteBook(id:any){
+    this.http.delete(`http://localhost:3000/books/${id}`).subscribe((data)=>{console.log(data)})
+  }
+  constructor(public http:HttpClient){
+    this.getBooks();
   }
 }
