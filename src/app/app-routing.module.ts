f5f6f793c7625d2c0e222a3dd638fc6d0a434082
edupoly <info.edupoly@gmail.com>
2022-11-07 10:37:16 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AngularComponent } from './angular/angular.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { ReactComponent } from './react/react.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutusComponent
+  },
+  {
+    path:'courses',
+    component:CoursesComponent,
+    children:[
+      {
+        path:'angular',
+        component:AngularComponent
+      },
+      {
+        path:'react',
+        component:ReactComponent
+      },
+    ]
+  },
+  {
+    path:'contactus',
+    component:ContactusComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
+  },
+  {
+    path:'countries',
+    component:CountriesComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ServicesComponent } from './services/services.component';
+import { CoursesComponent } from './courses/courses.component';
+import { AngularComponent } from './angular/angular.component';
+import { ReactComponent } from './react/react.component';
+import { CountriesComponent } from './countries/countries.component';
+import { HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactusComponent,
+    ServicesComponent,
+    CoursesComponent,
+    AngularComponent,
+    ReactComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
