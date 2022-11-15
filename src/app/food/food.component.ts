b1c7a1700acaafa881d61e22e95f7d58f7767770
edupoly <info.edupoly@gmail.com>
2022-11-15 23:00:56 +0530

    rxjs operators

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { retry,delay,take, debounceTime, map } from 'rxjs';
+import { retry,delay,take, debounceTime, map, filter,of, retryWhen, catchError, repeat } from 'rxjs';
 
 @Component({
   selector: 'app-food',
@@ -9,20 +9,40 @@ import { retry,delay,take, debounceTime, map } from 'rxjs';
 })
 export class FoodComponent implements OnInit {
 
-  constructor(public http:HttpClient) { }
+  constructor(public http:HttpClient) { 
+  }
   foodItems:any;
   ngOnInit(): void {
     this.http.get("http://localhost:3000/foodItems")
     .pipe(
 
-      map((restaurants:any)=>{
-        console.log(restaurants)
-        return restaurants.map((restaurant:any)=>{
-          return {...restaurant,restaurant:restaurant.restaurant.toUpperCase()}
-        })
+      //with retryWhen
+      retryWhen(err=>{
+        return err.pipe(
+          delay(4000),
+          take(5)
+        )
       })
     )
-    .subscribe((res)=>this.foodItems=res)
+    .subscribe((res)=>this.foodItems=res);
+
+    // this.http.get("http://localhost:3000/foodItems")
+    // .pipe(
+    //   map((restaurants:any)=>{
+    //     console.log(restaurants)
+    //     return restaurants.map((restaurant:any)=>{
+    //       return {...restaurant,restaurant:restaurant.restaurant.toUpperCase()}
+    //     })
+    //   })
+    // )
+    // .subscribe((res)=>this.foodItems=res);
+    
+    // //example of filter
+    // of(12,23,34,22,3,5,4,6).pipe(
+    //   filter(a=>a%2===0)
+    // ).subscribe(res=>console.log(res))
+
   }
+
 
 }
